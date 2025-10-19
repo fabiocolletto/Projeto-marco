975fdefe7dcb4ecaa74e02d6a86511c9479f70ca
Fabio Colletto <149891035+fabiocolletto@users.noreply.github.com>
2025-10-19 04:05:34 -0300

    fix: carregar painel do usuário sem build prévio

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -12,3 +12,22 @@ await fs.copy(path.join(rootDir, 'index.html'), path.join(distDir, 'index.html')
 await fs.copy(path.join(rootDir, 'miniapps'), path.join(distDir, 'miniapps'), {
   overwrite: true,
 });
+
+const vendorSource = path.join(rootDir, 'node_modules', 'idb', 'build');
+const vendorTarget = path.join(distDir, 'vendor', 'idb');
+if (await fs.pathExists(vendorSource)) {
+  await fs.ensureDir(vendorTarget);
+  await fs.copy(vendorSource, vendorTarget, { overwrite: true });
+}
+
+const indexPath = path.join(distDir, 'index.html');
+if (await fs.pathExists(indexPath)) {
+  const html = await fs.readFile(indexPath, 'utf-8');
+  const nextHtml = html.replace(
+    './node_modules/idb/build/index.js',
+    './vendor/idb/index.js',
+  );
+  if (nextHtml !== html) {
+    await fs.writeFile(indexPath, nextHtml, 'utf-8');
+  }
+}
